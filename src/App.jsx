d7a0c11a57d6a6ded5f31d736bf140717189747d
tetorapod-a <65341661+tetorapod-a@users.noreply.github.com>
2020-07-17 20:26:33 +0900

    chats&answers表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from "./dataset";
 import './assets/styles/style.css';
+import { AnswersList, Chats } from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,13 +13,57 @@ export default class App extends React.Component {
         dataset: defaultDataset,
         open: false
     }
+    this.selectAnswer = this.selectAnswer.bind(this)
+  }
+
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    })
+
+    this.setState({
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
+    })
+  }
+
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId) 
+        break;
+      default:
+        const chats = this.state.chats;
+        chats.push  ({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+            
+        this.setState({
+          chats: chats
+        })
+        
+        this.displayNextQuestion(nextQuestionId)
+        break;
+    }
+  }
+
+  componentDidMount() {
+    const initAnswer = '';
+    this.selectAnswer(initAnswer, this.state.currentId)
   }
 
   render() {
     return (
       <section className='c-section'>
         <div className='c-box'> 
-
+        <Chats
+          chats={this.state.chats}
+        />
+          <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
         </div>
 
       </section>
