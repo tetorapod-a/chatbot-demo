81ddb40678803fc8602ac69b5d5c68e5511c4bb0
tetorapod-a <65341661+tetorapod-a@users.noreply.github.com>
2020-07-25 10:14:12 +0900

    r

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAwkSlmwTOjJ2O7JBWrhF_-SQSHjZ3-rck",
+  authDomain: "chatbot-demo-887d1.firebaseapp.com",
+  databaseURL: "https://chatbot-demo-887d1.firebaseio.com",
+  projectId: "chatbot-demo-887d1",
+  storageBucket: "chatbot-demo-887d1.appspot.com",
+  messagingSenderId: "1093172237117",
+  appId: "1:1093172237117:web:8f6902d4204cfa6477ab54",
+  measurementId: "G-F94MPVB2GX"
+};
+
+export default firebaseConfig
